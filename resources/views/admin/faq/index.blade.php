f1e291d581156fa3cd672fbb0220a4d3dbff2f69
Fenanders <yandi.fenanda247@gmail.com>
2025-10-06 21:29:04 +0700

    fix: faq index

--- resources/views/admin/faq/index.blade.php
+++ resources/views/admin/faq/index.blade.php
@@ -14,43 +14,52 @@
                             Create New Our Services
                         </a>
                     </div>
-                    <table class="min-w-full divide-y divide-gray-200">
+                    {{-- Added min-w-full and table-auto for better responsiveness and column sizing --}}
+                    <table class="min-w-full divide-y divide-gray-200 table-auto">
                         <thead class="bg-gray-50">
                             <tr>
-                                <th scope="col" class="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                <th scope="col" class="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider w-10">
                                     No
                                 </th>
-                                <th scope="col" class="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                {{-- Added w-1/4 to help with column sizing --}}
+                                <th scope="col" class="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider w-1/4">
                                     Our Services
                                 </th>
-                                <th scope="col" class="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                {{-- Added w-2/4 to help with column sizing --}}
+                                <th scope="col" class="px-3 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider w-2/4">
                                     Description
                                 </th>
-                                <th scope="col" class="px-3 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                   Actions
+                                <th scope="col" class="px-3 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider w-1/4">
+                                    Actions
                                 </th>
                             </tr>
                         </thead>
                         <tbody class="bg-white divide-y divide-gray-200">
                             @foreach ($faqs as $index => $faq)
                                 <tr>
-                                    <td>
-                                        <div class="text-sm text-gray-900"">{{ $index + 1}}</div>
+                                    {{-- Removed px-6 py-4 to be consistent with <th>'s smaller padding --}}
+                                    <td class="px-3 py-3">
+                                        <div class="text-sm text-gray-900">{{ $index + 1}}</div>
                                     </td>
-                                    <td class="px-6 py-4 whitespace-nowrap">
+                                    {{-- REMOVED whitespace-nowrap and used px-3 py-3 --}}
+                                    <td class="px-3 py-3">
                                         <div class="text-sm text-gray-900">{{ $faq->question }}</div>
                                     </td>
-                                    <td class="px-6 py-4 whitespace-nowrap">
+                                    {{-- REMOVED whitespace-nowrap and used px-3 py-3 --}}
+                                    <td class="px-3 py-3">
                                         <div class="text-sm text-gray-900">{{ $faq->answer }}</div>
                                     </td>
-                                    <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                        <a href="{{ route('admin.faq.show', $faq->id) }}" class="text-green-600 hover:text-green-900">View</a>
-                                        <a href="{{ route('admin.faq.edit', $faq->id) }}" class="text-indigo-600 hover:text-indigo-900 ml-4">Edit</a>
-                                        <form action="{{ route('admin.faq.destroy', $faq->id) }}" method="POST" class="inline-block">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" class="text-red-600 hover:text-red-900 ml-4">Delete</button>
-                                        </form>
+                                    {{-- Added flex and space-x-2 to make buttons fit better and wrapped with a <div> --}}
+                                    <td class="px-3 py-3 text-right text-sm font-medium">
+                                        <div class="flex justify-end space-x-2">
+                                            <a href="{{ route('admin.faq.show', $faq->id) }}" class="text-green-600 hover:text-green-900">View</a>
+                                            <a href="{{ route('admin.faq.edit', $faq->id) }}" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                                            <form action="{{ route('admin.faq.destroy', $faq->id) }}" method="POST" class="inline-block">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="text-red-600 hover:text-red-900">Delete</button>
+                                            </form>
+                                        </div>
                                     </td>
                                 </tr>
                             @endforeach
